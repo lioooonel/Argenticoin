2c5bbcd84a9171e2f7b8e14ba95346069e5242ac
lioooonel <lionel11are@gmail.com>
2024-06-07 12:40:56 -0300

    commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<link rel="icon" href="sol_logo.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="nav.css">
+<title>Argentcoin</title>
+<style>
+  
+</style>
+</head>
+<body>
+
+<div class="contenedor-logo">
+  <div class="logo"></div>
+  <div class="nombre-marca">ARGENTCON</div>
+</div>
+
+<nav>
+  <ul>
+    <li><a href="#">LA COLECCIÓN</a>
+      <div class="submenu">
+        <a href="#">Subopción 1</a>
+        <a href="#">Subopción 2</a>
+        <!-- Más subopciones si es necesario -->
+      </div>
+    </li>
+    <li><a href="#">GUARDADOS</a>
+      <div class="submenu">
+        <a href="#">Subopción 1</a>
+        <a href="#">Subopción 2</a>
+        <!-- Más subopciones si es necesario -->
+      </div>
+    </li>
+    <li><a href="#">NOSOTROS</a></li>
+    <li><a href="#">CONTACTANOS</a></li>
+    <li><a href="#">CUENTA/REGISTRE</a></li>
+  </ul>
+</nav>
+
+</body>
+</html>
